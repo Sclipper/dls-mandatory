2f36771cdd2659dc5ad95b073a6347b35e1c4cf5
Sclipper <mkopoi3@gmail.com>
2021-05-31 09:58:39 +0200

    stuff

--- src/Schemas/Code.js
+++ src/Schemas/Code.js
@@ -6,13 +6,7 @@ function addMinutes (date, minutes) {
 
 const codeSchema = mongoose.Schema({
   id: mongoose.Types.ObjectId,
-<<<<<<< HEAD
-  code: { type: String, unique: true, default: crypto.randomBytes(8).toString('hex') },
-  teacher: String,
-  classId: String,
-=======
   code: { type: String, unique: true },
->>>>>>> 508a71e5f487d2fad3d8f033c878a81e20928d9b
   expires_at: { type: Date, default: addMinutes(new Date(), 20) },
   subject_id: mongoose.Types.ObjectId,
 }, { timestamps: true })
