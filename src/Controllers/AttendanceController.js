e3c13cf504a2e5a38a9f9923b8982479f9f9f63f
Sclipper <mkopoi3@gmail.com>
2021-06-06 21:26:38 +0200

    get all attendance

--- src/Controllers/AttendanceController.js
+++ src/Controllers/AttendanceController.js
@@ -1,15 +1,30 @@
+/* eslint-disable no-underscore-dangle */
 import { Router } from 'express'
-import Attendance from '../Models/AttendanceModel'
+import AttendanceModel from '../Models/AttendanceModel'
+import Attendance from '../Schemas/Attendance'
 import User from '../Schemas/User'
 import Code from '../Schemas/Code'
+import { findUser } from '../Queries/findUser'
 
 const AttendanceController = Router()
 
 AttendanceController.post('/token', async (req, res) => {
   try {
     const { subjectId } = req.body
-    const code = await Attendance.createToken(subjectId)
+    const code = await AttendanceModel.createToken(subjectId)
     res.send({ code: code.code })
+  } catch (err) {
+    console.log('Error', err)
+  }
+})
+AttendanceController.post('/', async (req, res) => {
+  try {
+    const { email } = req.body
+    const users = await findUser({ email })
+    const user = users[0]
+    Attendance.find({}, (err, attendance) => {
+      res.send(attendance.filter(x => x.student_id.toString() === user._id.toString()))
+    })
   } catch (err) {
     console.log('Error', err)
   }
@@ -24,7 +39,8 @@ AttendanceController.post('/register', async (req, res) => {
     const userCode = await Code.find({ code })
     console.log('code', userCode)
     if (userCode[0].expires_at > new Date()) {
-      const attendance = await Attendance.registerAttendance(studentId, subjectId, userCode[0].code)
+      const attendance = await AttendanceModel
+        .registerAttendance(studentId, subjectId, userCode[0].code)
       res.send(attendance)
     } else {
       res.send({ error: 'No code has been found' })
